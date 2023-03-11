ad02948d81950a94211bb17715b476cfcf91cbcd
Fernando Paulino <xizes2@hotmail.com>
2023-03-11 16:29:32 +0100

    rmve dotenv/loadEnv, config env and img path pattrn on next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    NEXT_PUBLIC_SANITY_TOKEN: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "/images/0pe18ibm/production/**",
+      },
+    ],
+  },
+};
