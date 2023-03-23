4e9c8791b7f00d3bec5f463b76ee622240cd4028
Fernando Paulino <xizes2@hotmail.com>
2023-03-23 11:12:27 +0100

    create footer component, add it to index

--- components/FooterBanner.tsx
+++ components/FooterBanner.tsx
@@ -1,7 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { urlFor } from "../lib/client";
+import { HeroBannerProps } from "./HeroBanner";
 
-function FooterBanner() {
-  return <div>FooterBanner</div>;
+function FooterBanner({
+  discount,
+  largeText1,
+  largeText2,
+  saleTime,
+  smallText,
+  midText,
+  description,
+  product,
+  buttonText,
+  image,
+}: HeroBannerProps) {
+  return (
+    <div>
+      <div className="footer-banner-container">
+        <div className="banner-desc">
+          <div className="left">
+            <p>{discount}</p>
+            <h3>{largeText1}</h3>
+            <h3>{largeText2}</h3>
+            <p>{saleTime}</p>
+          </div>
+          <div className="right">
+            <p>{smallText}</p>
+            <h3>{midText}</h3>
+            <p>{description}</p>
+            <Link href={`/product/${product}`}>
+              <button type="button">{buttonText}</button>
+            </Link>
+          </div>
+          <Image
+            src={urlFor(image && image.asset._ref).url()}
+            alt={description}
+            width={250}
+            height={250}
+            className="footer-banner-image"
+          ></Image>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FooterBanner;
