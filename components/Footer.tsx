0bbfa7bd2e82502ae9ccf3b835f3e804b5bc850e
Fernando Paulino <xizes2@hotmail.com>
2023-03-23 12:09:19 +0100

    create footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+import { AiFillInstagram } from "react-icons/ai";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <p>2023 FPN All rights reserved</p>
+      <span className="icons">
+        <Link href={"https://www.instagram.com/tutatisvet/"}>
+          <AiFillInstagram />
+        </Link>
+      </span>
+    </div>
+  );
+}
+
+export default Footer;
