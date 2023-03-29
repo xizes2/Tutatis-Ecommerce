5e11c98992dda7cb3187cc753b2eb05f4f1e7096
Fernando Paulino <xizes2@hotmail.com>
2023-03-29 14:31:04 +0200

    correct favicon path

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="favicon_tutatis.png" />
+        <link rel="icon" href="/favicon_tutatis.png" />
       </Head>
       <body>
         <Main />
