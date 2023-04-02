3b5a382ed0419069dd5b79e34b00e89b9387903b
Fernando Paulino <xizes2@hotmail.com>
2023-04-02 16:19:36 +0200

    delete logic/state that were transfered to context

--- hooks/useTutatisEcommerce.ts
+++ hooks/useTutatisEcommerce.ts
@@ -1,14 +1,12 @@
 import { useState } from "react";
-import { toast } from "react-hot-toast";
 
-interface ShopCart {
+export interface IProductAdded {
   productId: string;
   productQuantity: number;
 }
 
 function useTutatisEcommerce() {
   const [purchaseQuantity, setPurchaseQuantity] = useState(1);
-  const [shopCart, setShopCart] = useState(Array<ShopCart>);
 
   function increasePurchaseQuantity() {
     setPurchaseQuantity((prevQuantity) => {
@@ -25,43 +23,11 @@ function useTutatisEcommerce() {
     });
   }
 
-  function addProductToCart(productId: string, productQuantity: number) {
-    const addedProduct = {
-      productId: productId,
-      productQuantity: productQuantity,
-    };
-
-    const isProductOnShopCart = shopCart.some(
-      (productInCart) => productInCart.productId === addedProduct.productId
-    );
-
-    if (!isProductOnShopCart) {
-      setShopCart([...shopCart, addedProduct]);
-      toast.success("Producto agregado al carrito!");
-    } else {
-      const updatedCart = shopCart.map((productInCart) => {
-        if (productInCart.productId === addedProduct.productId) {
-          return {
-            ...productInCart,
-            productQuantity:
-              productInCart.productQuantity + addedProduct.productQuantity,
-          };
-        } else {
-          return productInCart;
-        }
-      });
-      setShopCart(updatedCart);
-      toast.success("Cantidad del producto actualizada!");
-    }
-  }
-
   return {
     purchaseQuantity,
-    shopCart,
     setPurchaseQuantity,
     increasePurchaseQuantity,
     decreasePurchaseQuantity,
-    addProductToCart,
   };
 }
 
