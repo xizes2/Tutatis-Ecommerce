7cc4e11d1d01930f2f1fd71308f6c3cf4cecb8f2
Fernando Paulino <xizes2@hotmail.com>
2023-04-02 16:30:41 +0200

    create shopcart component

---
+++ src/pages/shopcart/shopcart.tsx
@@ -0,0 +1,24 @@
+import React, { useContext, useState } from "react";
+import {
+  IShopCartContext,
+  ShopCartContext,
+} from "../../../context/ShopCartContext";
+
+function ShopCart() {
+  const { shopCart } = useContext(ShopCartContext) as IShopCartContext;
+
+  return (
+    <div>
+      ShopCart
+      <div>
+        {shopCart.map((productOnShopCart) => (
+          <div key={productOnShopCart.productId}>
+            {productOnShopCart.productId}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ShopCart;
