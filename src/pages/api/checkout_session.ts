09e977b9da661b5d3e1323af24c06a1dc044bad1
Fernando Paulino <xizes2@hotmail.com>
2023-04-10 22:14:36 +0200

    create checkout session handler

--- src/pages/api/checkout_session.ts
+++ src/pages/api/checkout_session.ts
@@ -1,7 +1,9 @@
+import { IProductAdded } from "../../../context/ShopCartContext";
+
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(
-  req: { method: string; headers: { origin: any } },
+  req: { method: string; headers: { origin: any }; body: Array<IProductAdded> },
   res: {
     redirect: (arg0: number, arg1: any) => void;
     status: (arg0: number) => {
@@ -16,13 +18,37 @@ export default async function handler(
   if (req.method === "POST") {
     try {
       const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price: "{{PRICE_ID}}",
-            quantity: 1,
-          },
-        ],
+        line_items: req.body.map((product) => {
+          const img = product.productImage
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/0pe18ibm/production/"
+            )
+            .replace(/-(jpg|png|webp|jpeg)$/i, ".$1");
+
+          return {
+            price_data: {
+              currency: "eur",
+              product_data: {
+                name: product.productName,
+                images: [img],
+              },
+              unit_amount: product.productPrice * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: product.productQuantity,
+          };
+        }),
         mode: "payment",
+        shipping_options: [
+          {
+            shipping_rate_data: "shr_1MvKLFHcRskR6ICcZmk6qdkq",
+          },
+          ,
+        ],
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       });
